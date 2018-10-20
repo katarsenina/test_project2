364df31d4b29a5e296df132dd56abb8852f16eec
Kat_Arsenina <37741916+katarsenina@users.noreply.github.com>
2018-10-20 12:49:28 +0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,21 @@
+def enter_word():
+    ''' Ввод слова без пробелов всередине и других небуквенных символов.
+     str -> str'''
+    while True:
+        s = input("Enter word: ").strip()
+        if s.isalpha():
+            return s
+
+
+
+def join_lists(list1, list2, list3):
+    '''Соединяет 3 списка  одинакового типа в один
+    (list1, list2, list3) -> list'''
+    assert type(list1) is type(list2) is type(list3)
+    join_lists = list1 + list2 + list3
+    return join_lists
+
+
+
+if __name__ == "__main__":
+    print()
